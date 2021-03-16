faa27ba1b964f30f2bba344e55a3280ec6820a57
hass0n3 <hassen.alfaris@ocsbstudent.ca>
2021-03-16 16:32:11 +0000

    ,

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+// this is code making a bubootm click and make it send a message saying ("hello word")
 document.getElementById('message').addEventListener
 ('click', displayMessage)
-
+// in this im cominding it to displayMessage saying ("hello word")
 function displayMessage () {
 	alert("Hello Word ")
 }
+
+document.getAnimations("message").addEventListener
+('click',displayMessage)
+function displayMessage()[
+document.getElementById('paragraph').innerHTML='hello word'
+
+]
